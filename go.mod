846203a6a0beb1b6774992efd9ae435929ec99de
yuta-2001 <yu0210ta.sug@gmail.com>
2024-01-09 19:34:50 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-todo-2
+
+go 1.20
+
+require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"go-todo-2/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
