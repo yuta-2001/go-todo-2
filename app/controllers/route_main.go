eefd30d9b947fbc69c731cfbf86da8df25539cf4
yuta-sugi <sugimine.yuta@codecomplete.com>
2024-01-15 22:10:33 +0700

    server layout

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -2,10 +2,8 @@ package controllers
 
 import (
 	"net/http"
-	"html/template"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseFiles("app/views/templates/top.html"))
-	tpl.Execute(w, nil)
+	generateHTML(w, "Hello", "layout", "top")
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,22 @@
 package controllers
 
 import (
-	"net/http"
+	"fmt"
+	"html/template"
 	"go-todo-2/config"
+	"net/http"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
